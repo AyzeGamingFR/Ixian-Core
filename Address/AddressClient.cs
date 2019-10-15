e1faa70fd7f77a96e363cfbcd7bb561cd54291be
Marko Zagar <ashkante@gmail.com>
2019-10-15 15:17:53 +0200

    Changed AddressClient so it can generate the same address always. Note: Users of AddressClient will have to be updated.

--- Address/AddressClient.cs
+++ Address/AddressClient.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace IXICore
 {
     /// <summary>
-    ///  Allows a client to generated a secret value with incorporated markers for multiple addresses.
+    ///  Allows a client to generate a secret value with incorporated markers for multiple addresses.
     ///  Such a value obscures which addresses it represents, but allows others to check whether a given address is part of the set
     ///  or not with a configurable degree of false-positives.
     ///  See also `AddressMatcher`.
@@ -24,10 +24,35 @@ namespace IXICore
     public class AddressClient
     {
         private List<byte[]> addresses;
+        private int randomSeedValue;
+        private Random rng = null;
 
         public AddressClient()
         {
             addresses = new List<byte[]>();
+            randomSeedValue = 0;
+            rng = new Random();
+        }
+
+        /// <summary>
+        /// Creates an address matcher generator with the specified entropy bytes.
+        ///  This will cause the matcher to always return the same matcher value if the same addresses are input,
+        ///  thereby preventing some types of statistical analysis a malicious DLT or S2 node could perform by
+        ///  asking the client to repeatedly generate new matchers.
+        /// </summary>
+        /// <remarks>
+        ///  In order for the same matcher values to be generatd, the entropy bytes must also be the same.
+        ///  This value must be preserved between client starts. A good candidate here is part of the private key (d value).
+        /// </remarks>
+        /// <param name="entropy">Bytes with some random state.</param>
+        public AddressClient(byte[] entropy)
+        {
+            addresses = new List<byte[]>();
+            byte[] hash = Crypto.sha512sqTrunc(entropy);
+            uint offset_into = BitConverter.ToUInt32(hash, 0);
+            offset_into = (uint)(offset_into % hash.Length);
+            randomSeedValue = BitConverter.ToInt32(hash, (int)offset_into);
+            rng = new Random(randomSeedValue);
         }
 
         /// <summary>
@@ -68,9 +93,9 @@ namespace IXICore
             return addresses.Count;
         }
 
-        private int getRandomUnfilledByte(Random rnd, byte[] filled)
+        private int getRandomUnfilledByte(byte[] filled)
         {
-            int possible_rnd = rnd.Next(filled.Length) + 1;
+            int possible_rnd = rng.Next(filled.Length) + 1;
             int i = 0;
             while (true)
             {
@@ -88,17 +113,14 @@ namespace IXICore
         ///  Generates the 'matcher' value (see description of this class `AddressClient`.
         /// </summary>
         /// <remarks>
-        ///  The random generator for this function doesn't strictly have to be cryptographically secure, but it is recommended to use
-        ///  a cryptographically secure random if possible.
         ///  The `bytes_per_addr` value determines how many false positives are likely to occur when matching random addresses. A smaller
         ///  value will produce more false positives, straining the bandwith and making the client process more data, while a higher number will
         ///  yield fewer false positives, but give away client's addresses with more certainty. 2-4 are usually good values.
         ///  Please note that both parties must agree on what value to use in `bytes_per_addr` in order for the matcher to work.
         /// </remarks>
-        /// <param name="rnd">Source of entropy - this should be pseudo-random, but doesn't need to be cryptographically secure.</param>
         /// <param name="bytes_per_addr">How many bytes should be used to encode each address. See remarks.</param>
         /// <returns>Matcher value, which can be used to determine if any random address was included in this matcher's set.</returns>
-        public List<byte[]> generateHiddenMatchAddresses(Random rnd, int bytes_per_addr)
+        public List<byte[]> generateHiddenMatchAddresses(int bytes_per_addr)
         {
             List<byte[]> matchers = new List<byte[]>();
             byte[] matcher = new byte[48];
@@ -106,7 +128,11 @@ namespace IXICore
             for (int i = 0; i < filled.Length; i++) { filled[i] = 0; }
             filled[0] = 1; // Never use the first byte for this
             // Start with initial random state
-            rnd.NextBytes(matcher);
+            if (randomSeedValue != 0)
+            {
+                rng = new Random(randomSeedValue);
+            }
+            rng.NextBytes(matcher);
 
             foreach (var addr in addresses)
             {
@@ -115,13 +141,13 @@ namespace IXICore
                     // Matcher is full
                     matchers.Add(matcher);
                     matcher = new byte[48];
-                    rnd.NextBytes(matcher);
+                    rng.NextBytes(matcher);
                     for (int i = 0; i < filled.Length; i++) { filled[i] = 0; }
                     filled[0] = 1;
                 }
                 for (int j = 0; j < bytes_per_addr; j++)
                 {
-                    int b = getRandomUnfilledByte(rnd, filled);
+                    int b = getRandomUnfilledByte(filled);
                     matcher[b] = addr[b];
                     filled[b] = 1;
                 }
